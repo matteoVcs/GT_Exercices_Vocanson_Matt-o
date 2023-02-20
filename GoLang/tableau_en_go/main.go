699866f2bcd4b362993b419c3285d0d75dd94826
matteo Vocanson <matteo.vocanson@ynov.com>
2023-02-20 21:57:28 +0100

    {+}tableau_en_go{+}

---
+++ GoLang/tableau_en_go/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"math/rand"
+	"net/http"
+	"text/template"
+	"time"
+)
+
+type Global struct {
+	N       int
+	Contenu []int
+}
+
+func main() {
+	var g Global
+	fs := http.FileServer(http.Dir("css"))
+	http.Handle("/css/", http.StripPrefix("/css/", fs))
+	http.HandleFunc("/", g.Page)
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func (g *Global) InitStruct(r *http.Request, w http.ResponseWriter) []int {
+	var clear []int
+	g.Contenu = clear
+	var max int = 100
+	var min int = 1
+	rand.Seed(time.Now().UnixNano())
+	g.N = rand.Intn(max-min) + min
+	for i := 0; i < g.N; i++ {
+		g.Contenu = append(g.Contenu, rand.Int())
+	}
+	return g.Contenu
+}
+
+func (g *Global) Page(w http.ResponseWriter, r *http.Request) {
+	tmp := template.Must(template.ParseFiles("index.html"))
+	details := Global{
+		Contenu: g.InitStruct(r, w),
+	}
+	tmp.Execute(w, details)
+}
